05c323043c27b9ec8c5dfc61f9c9191043a35afe
Daniel Abril <danbril1015@gmail.com>
2023-12-29 15:23:56 -0500

    change credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
+// import { ConfigModule } from '@nestjs/config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'userscrud',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [User],
       synchronize: true,
     }),
